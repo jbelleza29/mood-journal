8b68b76a1361c252698ac232fbc180a0822a576b
Pritz Belleza <pritz.belleza@gmail.com>
2023-11-24 16:49:14 -0500

    update ai section for mood journal

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -18,9 +18,35 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id);
+  const analysisData = [
+    { name: 'Subject', value: '' },
+    { name: 'Summary', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: 'False' },
+  ];
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                className="flex items-center justify-between px-2 py-2 border-b border-t border-black/10"
+                key={item.name}
+              >
+                <span className="text-lg font-semibold ">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
